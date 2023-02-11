1490e92641c8494010b4963001b8ff0548541b47
Piotr <122234066+Piotr0893@users.noreply.github.com>
2023-02-11 20:00:13 +0100

    Fixing typo

--- EarthQuakeApp/urls.py
+++ EarthQuakeApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.main, name='home'),
     path('EarthQuakes', views.Last24EarthQuakes, name ='EarthQuakes'),
     path("Last1Hour", views.Last1Hour, name='Last1Hour'),
-    path('Last7Days', views.Last7days, name='Last7Days')
+    path('Last7Days', views.Last7Days, name='Last7Days')
     
     
 ]
