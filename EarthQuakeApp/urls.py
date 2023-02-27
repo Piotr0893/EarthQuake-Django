46b0b54dcec3a9e59594539e57633ce2cf9d6ef0
Piotr <122234066+Piotr0893@users.noreply.github.com>
2023-02-27 17:53:15 +0100

    Add files via upload

--- EarthQuakeApp/urls.py
+++ EarthQuakeApp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('Last24Hours', views.Last24Hours, name ='Last24Hours'),
     path("Last1Hour", views.Last1Hour, name='Last1Hour'),
     path('Last7Days', views.Last7Days, name='Last7Days'), 
+    path('Last24hMap', views.Last24hMap, name ='Last24hMap'),
+    path('Last1hMap', views.Last1hMap, name ='Last1hMap'),
+    path('Last7dMap', views.Last7dMap, name ='Last7dMap'),
 ]
--- EarthQuakeApp/views.py
+++ EarthQuakeApp/views.py
@@ -1,9 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import json
 import urllib
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import pandas as pd
 
 
 # Create your views here.
+def Last1hMap(request):
+    m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80,
+                    llcrnrlon=-180, urcrnrlon=180, resolution='c')
+    m.drawcoastlines()
+    m.drawcountries()
+    path = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.csv"  # this is the .csv file
+    data1 = pd.read_csv(path)             # reading the data set 
+    lon = []                            # extracting information from data set
+    for i in data1['longitude']:
+        lon.append(i)
+    lat = []
+    for i in data1['latitude']:
+        lat.append(i)
+    x, y = m(lon, lat)                   #converting longitude ,laitude to x ,y
+    sizes = data1['mag'].values ** 2     # different size of plot point
+    x = np.array(x, dtype=float)
+    y = np.array(y, dtype=float)
+    sizes = np.array(sizes, dtype=float)
+    m.scatter(x, y, marker='o', c=data1['mag'], s=sizes, label=data1['mag'])     #plotting the data
+    plt.show()
+    return redirect ('Last1Hour')
+
+def Last7dMap(request):
+    m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80,
+                    llcrnrlon=-180, urcrnrlon=180, resolution='c')
+    m.drawcoastlines()
+    m.drawcountries()
+    path = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv"  # this is the .csv file
+    data1 = pd.read_csv(path)             # reading the data set 
+    lon = []                            # extracting information from data set
+    for i in data1['longitude']:
+        lon.append(i)
+    lat = []
+    for i in data1['latitude']:
+        lat.append(i)
+    x, y = m(lon, lat)                   #converting longitude ,laitude to x ,y
+    sizes = data1['mag'].values ** 2     # different size of plot point
+    x = np.array(x, dtype=float)
+    y = np.array(y, dtype=float)
+    sizes = np.array(sizes, dtype=float)
+    m.scatter(x, y, marker='o', c=data1['mag'], s=sizes, label=data1['mag'])     #plotting the data
+    plt.show()
+    return redirect ('Last7Days')
+
+def Last24hMap(request):
+    m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80,
+                    llcrnrlon=-180, urcrnrlon=180, resolution='c')
+    m.drawcoastlines()
+    m.drawcountries()
+    path = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv"  # this is the .csv file
+    data1 = pd.read_csv(path)             # reading the data set 
+    lon = []                            # extracting information from data set
+    for i in data1['longitude']:
+        lon.append(i)
+    lat = []
+    for i in data1['latitude']:
+        lat.append(i)
+    x, y = m(lon, lat)                   #converting longitude ,laitude to x ,y
+    sizes = data1['mag'].values ** 2     # different size of plot point
+    x = np.array(x, dtype=float)
+    y = np.array(y, dtype=float)
+    sizes = np.array(sizes, dtype=float)
+    m.scatter(x, y, marker='o', c=data1['mag'], s=sizes, label=data1['mag'])     #plotting the data
+    plt.show()
+    return redirect ('Last24Hours')
+
 def Last24Hours(request):
     urlData ="http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"      # URL for the earthquake data API  
     webURL = urllib.request.urlopen(urlData)                                                     
